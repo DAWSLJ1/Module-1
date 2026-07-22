eccd6508eff1e31fc9a5f5207482d1f6e172e9d1
Luke Dawson (1000141012) <DAWSLJ1@student.op.ac.nz>
2026-07-22 14:23:38 +1200

    VS files

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Input a number");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+                Console.ReadLine();
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+                Console.ReadLine();
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+                Console.ReadLine();
+            }
+            else
+            {
+                    Console.WriteLine(number);
+                    Console.ReadLine();              
+            }
+
+
+
+        }
+    }
+}
